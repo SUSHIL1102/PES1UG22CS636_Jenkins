5641f9de506ca757487a3b3b9d34f0ed3e06255f
SUSHIL1102 <121409003+SUSHIL1102@users.noreply.github.com>
2025-03-13 17:15:12 +0530

    Create new.cpp

---
+++ main/new.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES1UG22CS636 \n";
+  cout << "This is my new file \n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this \n";
+  return 0;
+}
